9a26d9124be412e85b6409617f2b90d904daca85
Evren Ustun <ustunnevren@gmail.com>
2022-07-29 16:37:03 +0300

    Prepared profile page.

---
+++ Instagram/Instagram/Views/Cells/Home Feed/IGFeedPostGeneralTableViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  IGFeedPostGeneralTableViewCell.swift
+//  Instagram
+//
+//  Created by Evren Ustun on 23.07.2022.
+//
+
+import UIKit
+
+class IGFeedPostGeneralTableViewCell: UITableViewCell {
+
+
+
+}
---
+++ Instagram/Instagram/Views/Cells/Home Feed/IGFeedPostHeaderTableViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  IGFeedPostHeaderTableViewCell.swift
+//  Instagram
+//
+//  Created by Evren Ustun on 23.07.2022.
+//
+
+import UIKit
+
+class IGFeedPostHeaderTableViewCell: UITableViewCell {
+
+
+
+}
